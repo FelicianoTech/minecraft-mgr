f35322916a9e8ce390e10fe8ae24fcb8da55093e
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2026-02-04 10:04:59 -0500

    Add support for Ubuntu 26.04

--- minecraft-mgr.sh
+++ minecraft-mgr.sh
@@ -24,6 +24,9 @@ install_minecraft(){
 		noble)
 			pkgs="default-jre libgdk-pixbuf2.0-0"
 			;;
+		resolute)
+			pkgs="default-jre libgdk-pixbuf2.0-0"
+			;;
 		*)
 			echo "Your operating system is unsupported."
 			exit 1
